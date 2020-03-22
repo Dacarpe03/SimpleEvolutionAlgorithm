362c4a9c9c1fe3c7bdd1b0369f7d2c941a39aa30
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-03-22 20:45:47 +0100

    Working algorithm

--- Main.py
+++ Main.py
@@ -12,9 +12,15 @@ def initializePopulation(size, lenString, objString):
     pop = []
     for i in range(size):
         gen = randomString(lenString)
-        m = Member(gen)
+        newMember = Member(gen)
+        newMember.updateFitnesses(objString)
+        pop.append(newMember)
+    return pop
+
+
+def updFit(pop, objString):
+    for m in pop:
         m.updateFitnesses(objString)
-        pop.append(m)
     return pop
 
 
@@ -25,17 +31,52 @@ def calculateTotalFitness(memPop):
     return total
 
 
+def initializeNormFitness(population, total):
+    for m in population:
+        m.calculateNormFitness(total)
+
+
 if __name__ == "__main__":
 
-    objectiveString = "Me llamo Daniel"
-    iterations = 10
+    objectiveString = "Esta no es una frase larga"
+    iterations = 10000
 
-    populationSize = 500
+    populationSize = 250
     sizeString = len(objectiveString)
 
-    population = initializePopulation(populationSize, sizeString)
-
-    totalFitness = calculateTotalFitness(population)
+    population = initializePopulation(populationSize, sizeString, objectiveString)
+    auxPop = []
 
     for i in range(iterations):
-        print("HAHAH")
+
+        population = updFit(population, objectiveString)
+        totalFitness = calculateTotalFitness(population)
+        initializeNormFitness(population, totalFitness)
+
+        auxPop.clear()
+
+        memberOne = population[0]
+        memberTwo = population[1]
+
+        while len(auxPop) < populationSize:
+            for member in population:
+                if member.normFitness > memberOne.normFitness:
+                    memberOne = member
+                    population.remove(member)
+                elif member.normFitness > memberTwo.normFitness:
+                    memberTwo = member
+                    population.remove(member)
+
+            children = memberOne.reproduce(memberTwo)
+            auxPop.append(memberOne)
+            auxPop = auxPop + children
+
+        print("Best in iteration", i, ":", auxPop[0].genes)
+        population = auxPop.copy()
+
+
+    for member in population:
+        if member.genes == objectiveString:
+            print(member.genes)
+
+
--- Member.py
+++ Member.py
@@ -6,6 +6,7 @@ import string
 class Member:
 
     MUTATION_PROB = 0.3
+    NUMBER_OF_MUTATIONS = 2
 
     def __init__(self, genes):
         self.genes = genes
@@ -18,24 +19,30 @@ class Member:
         length = len(self.genes)
         position = random.randrange(length)
 
-        childOne = Member(self.genes[:-position] + otherMember.genes[:position])
-        childTwo = Member(otherMember.genes[:-position] + self.genes[:position])
+        childOne = Member(self.genes[position:] + otherMember.genes[:position])
+        childTwo = Member(otherMember.genes[position:] + self.genes[:position])
+
+        childOne.mutate()
+        childTwo.mutate()
 
         finalList = [childOne, childTwo]
         return finalList
 
     def mutate(self):
-        prob = random.uniform(0, 1)
-        if prob <= self.MUTATION_PROB:
-            length = len(self.genes)
-            position = random.randrange(length)
-            randomLetter = random.choice(string.letters + " ")
-            self.genes = self.genes[:position-1] + randomLetter + self.genes[position:]
+        for i in range(self.NUMBER_OF_MUTATIONS):
+            prob = random.uniform(0, 1)
+            if prob <= self.MUTATION_PROB:
+                length = len(self.genes)
+                position = random.randrange(length)
+                if position == 0:
+                    position = 1
+                randomLetter = random.choice(string.ascii_letters + " ")
+                self.genes = self.genes[:position - 1] + randomLetter + self.genes[position:]
 
     def calculateRawFitness(self, objective):
-        length = len(self.genes)
-        difLetters = sum(self.genes[i] != objective[i] for i in range(length))
-        self.rawFitness = difLetters/length
+        length = len(objective)
+        sameLetters = sum(self.genes[i] == objective[i] for i in range(length))
+        self.rawFitness = sameLetters/length
 
     def calculateScaleFitness(self):
         self.scaleFitness = 2**self.rawFitness
